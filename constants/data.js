303caa7222f587bc602720409e1d4fa11d2fede3
Hoang Son <soonhoang14@gmail.com>
2022-12-04 23:17:38 +0700

    feat: implement logic game

---
+++ constants/data.js
@@ -0,0 +1,127 @@
+const moneyData = [
+  {
+    index: "15",
+    money: 1000000000,
+  },
+  {
+    index: "14",
+    money: 500000000,
+  },
+  {
+    index: "13",
+    money: 250000000,
+  },
+  {
+    index: "12",
+    money: 125000000,
+  },
+  {
+    index: "11",
+    money: 64000000,
+  },
+  {
+    index: "10",
+    money: 32000000,
+  },
+  {
+    index: "09",
+    money: 16000000,
+  },
+  {
+    index: "08",
+    money: 8000000,
+  },
+  {
+    index: "07",
+    money: 4000000,
+  },
+  {
+    index: "06",
+    money: 2000000,
+  },
+  {
+    index: "05",
+    money: 1000000,
+  },
+  {
+    index: "04",
+    money: 500000,
+  },
+  {
+    index: "03",
+    money: 300000,
+  },
+  {
+    index: "02",
+    money: 200000,
+  },
+  {
+    index: "01",
+    money: 100000,
+  },
+];
+
+const moneyByNumberQuestion = [
+  {
+    index: 15,
+    money: 1000000000,
+  },
+  {
+    index: 14,
+    money: 500000000,
+  },
+  {
+    index: 13,
+    money: 250000000,
+  },
+  {
+    index: 12,
+    money: 125000000,
+  },
+  {
+    index: 11,
+    money: 64000000,
+  },
+  {
+    index: 10,
+    money: 32000000,
+  },
+  {
+    index: 9,
+    money: 16000000,
+  },
+  {
+    index: 8,
+    money: 8000000,
+  },
+  {
+    index: 7,
+    money: 4000000,
+  },
+  {
+    index: 6,
+    money: 2000000,
+  },
+  {
+    index: 5,
+    money: 1000000,
+  },
+  {
+    index: 4,
+    money: 500000,
+  },
+  {
+    index: 3,
+    money: 300000,
+  },
+  {
+    index: 2,
+    money: 200000,
+  },
+  {
+    index: 1,
+    money: 100000,
+  },
+];
+
+export { moneyData, moneyByNumberQuestion };
